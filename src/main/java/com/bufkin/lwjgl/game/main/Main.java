2c130ecc79ad7fa45806c924f29571adaa318de1
Bufkin <sebastian.schemer@gmail.com>
2019-10-26 09:46:18 +0200

    Finished episode 6
    Added slick utils library
    Added texture for testing purposes
    Added texture class
    Added rendering of the texture
    Added model class

--- src/main/java/com/bufkin/lwjgl/game/main/Main.java
+++ src/main/java/com/bufkin/lwjgl/game/main/Main.java
@@ -1,40 +1,65 @@
 package com.bufkin.lwjgl.game.main;
 
+import com.bufkin.lwjgl.game.Model;
+import com.bufkin.lwjgl.game.textures.Texture;
 import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GL46;
 
 import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
 
 public class Main {
-
+    
     private Main() {
         if (!glfwInit()) {
             System.err.println("GLFW failed to initialize!");
             System.exit(1);
         }
-
+        
         long window = glfwCreateWindow(1280, 720, "Window", 0, 0);
         glfwShowWindow(window);
         glfwMakeContextCurrent(window);
         GL.createCapabilities();
-
+        
+        glEnable(GL_TEXTURE_2D);
+        
+        float[] vertices = new float[]{
+                -0.5f, 0.5f, 0,     // TOP LEFT     0
+                0.5f, 0.5f, 0,      // TOP RIGHT    1
+                0.5f, -0.5f, 0,     // BOTTOM RIGHT 2
+                -0.5f, -0.5f, 0,    // BOTTOM LEFT  3
+        };
+        
+        float[] texture = new float[]{
+                0, 0,
+                1, 0,
+                1, 1,
+                0, 1
+        };
+        
+        int[] indices = new int[]{
+                0, 1, 2,
+                2, 3, 0
+        };
+        
+        Model   model = new Model(vertices, texture, indices);
+        Texture tex   = new Texture("./res/test.png");
+        
         while (!glfwWindowShouldClose(window)) {
+            if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_TRUE) {
+                glfwSetWindowShouldClose(window, Boolean.TRUE);
+            }
+            
             glfwPollEvents();
-
-            GL46.glClear(GL46.GL_COLOR_BUFFER_BIT);
-            GL46.glBegin(GL46.GL_QUADS);
-            GL46.glColor4f(1, 0, 0, 0);
-            GL46.glVertex2f(-0.5f, 0.5f);
-            GL46.glVertex2f(0.5f, 0.5f);
-            GL46.glVertex2f(0.5f, -0.5f);
-            GL46.glVertex2f(-0.5f, -0.5f);
-            GL46.glEnd();
-
+            glClear(GL_COLOR_BUFFER_BIT);
+            
+            tex.bind();
+            model.render();
+            
             glfwSwapBuffers(window);
         }
         glfwTerminate();
     }
-
+    
     public static void main(String[] args) {
         new Main();
     }
