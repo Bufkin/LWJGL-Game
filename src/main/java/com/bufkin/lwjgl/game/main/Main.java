492f55372359d1583389e95de1cdcabdad5283fa
Sebastian Schemer <sebastian.schemer@gmail.com>
2019-11-01 20:33:59 +0100

    Episode 12
    My PC should be running stable from now on...
    Set initial window position to the center of the primary monitor
    Added setSize method in the window class
    Changed maven compiler to version 11 of Java

--- src/main/java/com/bufkin/lwjgl/game/main/Main.java
+++ src/main/java/com/bufkin/lwjgl/game/main/Main.java
@@ -14,105 +14,103 @@ import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL46.*;
 
 public class Main {
-    
-    private final int WIDTH  = 1280;
+
+    private final int WIDTH = 1280;
     private final int HEIGHT = 720;
-    
+
     private Main() {
         if (!glfwInit()) {
             System.err.println("GLFW failed to initialize!");
             System.exit(1);
         }
-        
+
         Window window = new Window();
+        window.setSize(this.WIDTH, this.HEIGHT);
         window.createWindow("Game");
-        
+
         GL.createCapabilities();
-        
+
         Camera camera = new Camera(this.WIDTH, this.HEIGHT);
         glEnable(GL_TEXTURE_2D);
-        
+
         float[] vertices = new float[]{
                 -0.5f, 0.5f, 0,     // TOP LEFT     0
                 0.5f, 0.5f, 0,      // TOP RIGHT    1
                 0.5f, -0.5f, 0,     // BOTTOM RIGHT 2
                 -0.5f, -0.5f, 0,    // BOTTOM LEFT  3
         };
-        
+
         float[] texture = new float[]{
                 0, 0,
                 1, 0,
                 1, 1,
                 0, 1
         };
-        
+
         int[] indices = new int[]{
                 0, 1, 2,
                 2, 3, 0
         };
-        
-        Model   model  = new Model(vertices, texture, indices);
-        Shader  shader = new Shader("shader");
-        Texture tex    = new Texture("./res/test.png");
-        
+
+        Model model = new Model(vertices, texture, indices);
+        Shader shader = new Shader("shader");
+        Texture tex = new Texture("./res/test.png");
+
         Matrix4f scale = new Matrix4f()
-                                 .translate(new Vector3f(100, 0, 0))
-                                 .scale(256);
+                .translate(new Vector3f(100, 0, 0))
+                .scale(256);
         Matrix4f target = new Matrix4f();
         camera.setPosition(new Vector3f(-100, 0, 0));
-        
+
         double frame_cap = 1.0 / 60.0;
-        
+
         double frame_time = 0;
-        int    frames     = 0;
-        
-        double time        = Timer.getTime();
+        int frames = 0;
+
+        double time = Timer.getTime();
         double unprocessed = 0;
-        
+
         while (!window.shouldClose()) {
             boolean can_render = false;
-            
+
             double time_2 = Timer.getTime();
             double passed = time_2 - time;
             unprocessed += passed;
             frame_time += passed;
-            
+
             time = time_2;
-            
+
             while (unprocessed >= frame_cap) {
                 unprocessed -= frame_cap;
                 can_render = true;
-                
+
                 target = scale;
-//                if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_TRUE) {
-//                    glfwSetWindowShouldClose(window, Boolean.TRUE);
-//                }
-                
+
                 glfwPollEvents();
-                
+
                 if (frame_time >= 1.0f) {
                     frame_time = 0;
                     System.out.println("FPS: " + frames);
                     frames = 0;
                 }
             }
-            
+
             if (can_render) {
                 glClear(GL_COLOR_BUFFER_BIT);
-                
+
                 tex.bind(0);
                 shader.bind();
                 shader.setUniform("sampler", 0);
                 shader.setUniform("projection", camera.getProjection().mul(target));
                 model.render();
-                
+
                 window.swapBuffers();
                 frames++;
             }
         }
         glfwTerminate();
     }
-    
+
     public static void main(String[] args) {
         new Main();
     }
--- src/main/java/com/bufkin/lwjgl/game/window/Window.java
+++ src/main/java/com/bufkin/lwjgl/game/window/Window.java
@@ -1,46 +1,58 @@
 package com.bufkin.lwjgl.game.window;
 
+import org.lwjgl.glfw.GLFWVidMode;
+
 import static org.lwjgl.glfw.GLFW.*;
 
 public class Window {
     private long window;
-    private int  width, height;
-    
+    private int width, height;
+
     public Window() {
-    
+
     }
-    
+
     public void createWindow(String title) {
         this.window = glfwCreateWindow(this.width, this.height, title, 0, 0);
-        
+
         if (this.window == 0) {
             throw new IllegalStateException("Failed to create window!");
         }
-        
+
+        GLFWVidMode vid = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        glfwSetWindowPos(this.window,
+                (vid.width() - this.width) / 2,
+                (vid.height() - this.height) / 2
+        );
         glfwShowWindow(this.window);
         glfwMakeContextCurrent(this.window);
     }
-    
+
+    public void setSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
     public boolean shouldClose() {
         return glfwWindowShouldClose(this.window);
     }
-    
+
     public void swapBuffers() {
         glfwSwapBuffers(this.window);
     }
-    
+
     public int getWidth() {
         return this.width;
     }
-    
+
     public void setWidth(int width) {
         this.width = width;
     }
-    
+
     public int getHeight() {
         return this.height;
     }
-    
+
     public void setHeight(int height) {
         this.height = height;
     }
